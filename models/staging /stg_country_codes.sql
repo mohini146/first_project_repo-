9cb1de52ca230b643c160f071349fef3a44fdc4c
mohini146 <kulkarnimohini146@gmail.com>
2025-07-24 19:36:20 +0530

    Update stg_country_codes.sql

--- models/staging /stg_country_codes.sql
+++ models/staging /stg_country_codes.sql
@@ -1,5 +1,4 @@
--- models/stg_country_codes.sql
-SELECT
-  country_code,
-  country_name
-FROM {{ ref('country_codes') }}
+-- models/staging/stg_country_codes.sql
+
+select *
+from {{ ref('country_codes') }}
