71988848bc5f0c596887625f1a9769672dee6722
mohini146 <kulkarnimohini146@gmail.com>
2025-07-25 22:11:58 +0530

    Update stg_customer.sql

--- models/staging /stg_customer.sql
+++ models/staging /stg_customer.sql
@@ -1,18 +1,5 @@
-with source as (
-
-    select * from {{ source('public', 'customer_raw') }}
-
-),
-
-renamed as (
-
-    select
-        id as customer_id,
-        name as customer_name,
-        email,
-        country
-    from source
-
-)
-
-select * from renamed
+SELECT
+    ID AS customer_id,
+    NAME,
+    COUNTRY
+FROM {{ source('public', 'customer_raw') }}
