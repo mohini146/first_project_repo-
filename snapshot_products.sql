5751ef671f659e22e05dee9b049fe88ee4259b6f
mohini146 <kulkarnimohini146@gmail.com>
2025-07-26 23:03:50 +0530

    Delete this file"

--- snapshot_products.sql
+++ snapshot_products.sql
@@ -1,18 +1,1 @@
-{% snapshot snapshot_products %}
-  {{
-    config(
-      target_schema='snapshots',
-      unique_key='product_id',
-      strategy='timestamp',
-      updated_at='updated_at'
-    )
-  }}
 
-  SELECT
-    product_id,
-    name,
-    price,
-    updated_at
-  FROM {{ source('raw', 'products') }}
-
-{% endsnapshot %}
